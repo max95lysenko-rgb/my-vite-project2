21c663f6a6b1b7b75bb70ecf42990f217e5f156d
max95lysenko-rgb <max95lysenko@gmail.com>
2026-03-26 17:23:47 +0300

    Add passive income based on pokemon weight

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import { RootState, AppDispatch } from './store';
 import { addMoney } from './store/userSlice';
-import type { RootState, AppDispatch } from './store';
+import { saveUserData } from './utils/storage';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -9,3 +15,42 @@ const App: React.FC = () => {
   const { activePokemon, collection } = useSelector((state: RootState) => state.pokemon);
 
   useEffect(() => {
+    const timer = setInterval(() => {
+      if (username) {
+        const currentPoke = activePokemon || collection[0];
+        if (currentPoke && currentPoke.weight) {
+          const income = Math.max(1, Math.floor(currentPoke.weight / 10));
+          dispatch(addMoney(income));
+        }
+      }
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [dispatch, username, activePokemon, collection]);
+
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      if (username) {
+        saveUserData(username, money, collection);
+      }
+    };
+    window.addEventListener('beforeunload', handleBeforeUnload);
+    return () => window.removeEventListener('beforeunload', handleBeforeUnload);
+  }, [username, money, collection]);
+
+  return (
+    <Router>
+      <div style={{ backgroundColor: '#121212', minHeight: '100vh', color: 'white', fontFamily: 'sans-serif' }}>
+        <Header />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route 
+            path="/" 
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            } 
+          />
+        </Routes>
+      </div>
